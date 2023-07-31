a3125d8296a6dc6795e0a30695632079c69d5373
hadzic20 <hadzic20@itu.edu.tr>
2023-07-31 16:24:35 +0300

    Add function mapper

--- main.cpp
+++ main.cpp
@@ -5,33 +5,54 @@
 
 using namespace std;
 
-map<string, string> dictionary;
-
-void set_key_value(string key, string value) {
-	if (!dictionary.insert(pair<string, string>(key, value)).second)
-		dictionary.find(key)->second = value;
-	cout << "OK" << endl;
-}
-
-string get_key(string key) {
-	auto itr = dictionary.find(key);
-	if (itr != dictionary.end())
-		return ("\"" + itr->second + "\"");
-	else
-		return ("(nil)");
-}
-
-void del_key(string key) {
-	if (dictionary.find(key) != dictionary.end()) {
-		dictionary.erase(key);
-		cout << "(integer) 1" << endl;
+class Store {
+	map<string, string> dictionary;
+	
+public:
+	void set_key_value(string key, string value) {
+		if (!dictionary.insert(pair<string, string>(key, value)).second)
+			dictionary.find(key)->second = value;
+		cout << "OK" << endl;
 	}
+	void get_key(string key) {
+		auto itr = dictionary.find(key);
+		if (itr != dictionary.end())
+			cout << ("\"" + itr->second + "\"") << endl;
+		else
+			cout << ("(nil)") << endl;
+	}
+
+	void del_key(string key) {
+		if (dictionary.find(key) != dictionary.end()) {
+			dictionary.erase(key);
+			cout << "(integer) 1" << endl;
+		}
+		else {
+			cout << "(nil)" << endl;
+		}
+	}
+};
+
+int function_mapper(Store *store, string query, string key, string value) {
+	if (!query.compare("q"))
+		return (1);
+	else if (!query.compare(""))
+		return (2);
 	else {
-		cout << "(nil)" << endl;
+		if (!query.compare("set"))
+			store->set_key_value(key, value);
+		else if (!query.compare("get"))
+			store->get_key(key);
+		else if (!query.compare("del"))
+			store->del_key(key);
+		else
+			cout << "(error) I'm sorry, I don't recognize that command" << endl;
+		return (0);
 	}
 }
 
 void repl() {
+	Store store;
 	while (1) {
 		string line;
 		string query;
@@ -43,31 +64,14 @@ void repl() {
 		getline(cin, line);
 		buff << line;
 		buff >> query >> key >> value;
-		if (query == "q")
+		int tmp = function_mapper(&store, query, key, value);
+		if (tmp == 1)
 			break;
-		if (query == "")
+		if (tmp == 2)
 			continue;
 		if (value[0] != '"' && buff >> temp)
 			cout << "(error) ERR Syntax error" << endl;
-		else if (!query.compare("set")) {
-			if (value[0] == '"') {
-				while (buff >> temp) {
-					value += " ";
-					value += temp;
-				}
-				value = value.substr(1, value.length() - 2);
-			}
-			set_key_value(key, value);
-		}
-		else if (!query.compare("get")) {
-			cout << get_key(key) << endl;
-		}
-		else if (!query.compare("del")) {
-			del_key(key);
-		}
-		else {
-			cout << "(error) I'm sorry, I don't recognize that command" << endl;
-		}
+		
 	}
 }
 
