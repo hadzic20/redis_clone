c1e995f088433cc3521069199b466380301740d5
hadzic20 <hadzic20@itu.edu.tr>
2023-08-08 12:31:57 +0200

    Add parse function and quote handling, resolve comments

--- main.cpp
+++ main.cpp
@@ -1,80 +1,102 @@
-#include <iostream>
-#include <sstream>
-#include <map>
 #include <cstring>
+#include <iostream>
+#include <map>
+#include <sstream>
 
 using namespace std;
 
-class Store {
-	map<string, string> dictionary;
-	
-public:
-	void set_key_value(string key, string value) {
-		if (!dictionary.insert(pair<string, string>(key, value)).second)
-			dictionary.find(key)->second = value;
-		cout << "OK" << endl;
-	}
-	void get_key(string key) {
-		auto itr = dictionary.find(key);
-		if (itr != dictionary.end())
-			cout << ("\"" + itr->second + "\"") << endl;
-		else
-			cout << ("(nil)") << endl;
-	}
+class Hash {
+  map<string, string> my_hash;
 
-	void del_key(string key) {
-		if (dictionary.find(key) != dictionary.end()) {
-			dictionary.erase(key);
-			cout << "(integer) 1" << endl;
-		}
-		else {
-			cout << "(nil)" << endl;
-		}
-	}
+ public:
+  string set(string key, string value) {
+    if (!my_hash.insert(pair<string, string>(key, value)).second) {
+      my_hash.find(key)->second = value;
+    }
+    return ("OK");
+  }
+  string get(string key) {
+    auto itr = my_hash.find(key);
+    if (itr != my_hash.end()) {
+      return ("\"" + itr->second + "\"");
+    } else {
+      return ("(nil)");
+    }
+  }
+
+  string del(string key) {
+    if (my_hash.find(key) != my_hash.end()) {
+      my_hash.erase(key);
+      return ("(integer) 1");
+    } else {
+      return ("(nil)");
+    }
+  }
 };
 
-int function_mapper(Store *store, string query, string key, string value) {
-	if (!query.compare("q"))
-		return (1);
-	else if (!query.compare(""))
-		return (2);
-	else {
-		if (!query.compare("set"))
-			store->set_key_value(key, value);
-		else if (!query.compare("get"))
-			store->get_key(key);
-		else if (!query.compare("del"))
-			store->del_key(key);
-		else
-			cout << "(error) I'm sorry, I don't recognize that command" << endl;
-		return (0);
-	}
+string* parse(string line) {
+  int iterator = 0;
+  int index = 0;
+  string* args = new string[line.length()];
+  if (line == "") {
+    return (NULL);
+  }
+  while (line[iterator] != '\0') {
+    while (line[iterator] == ' ' || line[iterator] == '\t') {
+      iterator++;
+    }
+    if (line[iterator] == '"') {
+      iterator++;
+      while (line[iterator] != '"') {
+        args[index] += line[iterator++];
+      }
+      iterator++;
+      index++;
+      continue;
+    }
+    while (line[iterator] != ' ' && line[iterator] != '\t' && line[iterator] != '\0') {
+      args[index] += line[iterator++];
+    }
+    index++;
+  }
+  return (args);
+}
+
+string function_mapper(Hash* hash, string* args) {
+  if (!args[0].compare("q"))
+    return ("quit");
+  else if (!args[0].compare(""))
+    return ("blank line");
+  else {
+    if (!args[0].compare("set")) {
+      return (hash->set(args[1], args[2]));
+    } else if (!args[0].compare("get")) {
+      return (hash->get(args[1]));
+    } else if (!args[0].compare("del")) {
+      return (hash->del(args[1]));
+    } else {
+      return ("(error) I'm sorry, I don't recognize that command");
+    }
+  }
 }
 
 void repl() {
-	Store store;
-	while (1) {
-		string line;
-		string query;
-		string key;
-		string value;
-		string temp;
-		stringstream buff;
-		cout << "> ";
-		getline(cin, line);
-		buff << line;
-		buff >> query >> key >> value;
-		int tmp = function_mapper(&store, query, key, value);
-		if (tmp == 1)
-			break;
-		if (tmp == 2)
-			continue;
-		if (value[0] != '"' && buff >> temp)
-			cout << "(error) ERR Syntax error" << endl;
-		
-	}
+  Hash hash;
+  while (1) {
+    string line;
+    cout << "> ";
+    getline(cin, line);
+    string* args = parse(line);
+    string tmp = function_mapper(&hash, args);
+    delete[] args;
+    if (tmp == "quit") {
+      break;
+    } else if (tmp == "blank line") {
+      continue;
+    } else {
+      cout << tmp << endl;
+    }
+  }
 }
 
-int main() {
-	repl();
-}
+int main() { repl(); }
