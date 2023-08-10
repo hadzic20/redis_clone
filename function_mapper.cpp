b13ae65618152e5952894eef4a9262d41cf44088
hadzic20 <hadzic20@itu.edu.tr>
2023-08-10 13:44:36 +0200

    fix in if else structure

--- function_mapper.cpp
+++ function_mapper.cpp
@@ -1,15 +1,19 @@
 #include "redis.hpp"
 
 string function_mapper(Hash* hash, vector<string> args) {
-  if (args.size() == 0)
+  if (args.size() == 0) {
     return ("blank line");
-  if (!args[0].compare("q"))
+  }
+  if (!args[0].compare("q")) {
     return ("quit");
+  }
   if (!args[0].compare("set")) {
     return (hash->set(args));
-  } else if (!args[0].compare("get")) {
+  }
+  if (!args[0].compare("get")) {
     return (hash->get(args));
-  } else if (!args[0].compare("del")) {
+  }
+  if (!args[0].compare("del")) {
     return (hash->del(args));
   }
   return ("(error) I'm sorry, I don't recognize that command");
