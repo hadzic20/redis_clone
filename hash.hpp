174e7641f2d7cbc06e20eecc9cb0fe8b8053b7bc
hadzic20 <hadzic20@itu.edu.tr>
2023-08-10 13:35:43 +0200

    Bug fix

--- hash.hpp
+++ hash.hpp
@@ -6,27 +6,40 @@ class Hash {
   map<string, string> my_hash;
 
  public:
-  string set(string key, string value) {
-    if (!my_hash.insert(pair<string, string>(key, value)).second) {
-      my_hash.find(key)->second = value;
+  string set(vector<string> args) {
+    if (args.size() < 3) {
+      args.push_back("");
+    }
+    if (!my_hash.insert(pair<string, string>(args[1], args[2])).second) {
+      my_hash.find(args[1])->second = args[2];
     }
     return ("OK");
   }
-  string get(string key) {
-    auto itr = my_hash.find(key);
+  string get(vector<string> args) {
+    if (args.size() < 2) {
+      string rtrn = "(error) wrong number of arguments (given ";
+      rtrn += (args.size() - 1 + '0');
+      rtrn += ", expected 1)";
+      return (rtrn);
+    }
+    auto itr = my_hash.find(args[1]);
     if (itr != my_hash.end()) {
       return ("\"" + itr->second + "\"");
-    } else {
-      return ("(nil)");
     }
+    return ("(nil)");
   }
 
-  string del(string key) {
-    if (my_hash.find(key) != my_hash.end()) {
-      my_hash.erase(key);
-      return ("(integer) 1");
-    } else {
-      return ("(nil)");
+  string del(vector<string> args) {
+    if (args.size() < 2) {
+      string rtrn = "(error) wrong number of arguments (given ";
+      rtrn += (args.size() - 1 + '0');
+      rtrn += ", expected 1)";
+      return (rtrn);
     }
+    if (my_hash.find(args[1]) != my_hash.end()) {
+      my_hash.erase(args[1]);
+      return ("(integer) 1");
+    }
+    return ("(nil)");
   }
 };
