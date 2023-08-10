8ce71b7535bec8328f850e51e11c8ee55e7d0701
hadzic20 <hadzic20@itu.edu.tr>
2023-08-10 13:35:43 +0200

    Bug fix

--- function_mapper.cpp
+++ function_mapper.cpp
@@ -7,11 +7,11 @@ string function_mapper(Hash* hash, vector<string> args) {
     return ("quit");
   else {
     if (!args[0].compare("set")) {
-      return (hash->set(args[1], args[2]));
+      return (hash->set(args));
     } else if (!args[0].compare("get")) {
-      return (hash->get(args[1]));
+      return (hash->get(args));
     } else if (!args[0].compare("del")) {
-      return (hash->del(args[1]));
+      return (hash->del(args));
     } else {
       return ("(error) I'm sorry, I don't recognize that command");
     }
